8d326ad7689927ea592af1b9f45bb3a766f4aa09
Zokerus <Zokerus@users.noreply.github.com>
2024-01-07 21:19:30 +0100

    add engine particles

--- Player.cs
+++ Player.cs
@@ -13,6 +13,9 @@ public partial class Player : RigidBody3D
 	private AudioStreamPlayer explosionAudio;
 	private AudioStreamPlayer winAudio;
 	private AudioStreamPlayer3D engineAudio;
+	private GpuParticles3D boosterParticles;
+    private GpuParticles3D leftboosterParticles;
+    private GpuParticles3D rightboosterParticles;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -20,6 +23,9 @@ public partial class Player : RigidBody3D
 		explosionAudio = GetNode<AudioStreamPlayer>("ExplosionAudio");
         winAudio = GetNode<AudioStreamPlayer>("WinAudio");
         engineAudio = GetNode<AudioStreamPlayer3D>("EngineAudio");
+		boosterParticles = GetNode<GpuParticles3D>("BoosterParticles");
+        leftboosterParticles = GetNode<GpuParticles3D>("LeftBoosterParticles");
+        rightboosterParticles = GetNode<GpuParticles3D>("RightBoosterParticles");
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,6 +34,7 @@ public partial class Player : RigidBody3D
 		if (Input.IsActionPressed("boost"))
 		{
 			ApplyCentralForce(MultiplyByFloat(Transform.Basis.Y,  (float)delta * thrust));
+			boosterParticles.Emitting = true;
 			if(!engineAudio.Playing)
 			{
 				engineAudio.Play();
@@ -36,16 +43,24 @@ public partial class Player : RigidBody3D
 		else
 		{
             engineAudio.Stop();
+			boosterParticles.Emitting = false;
         }
 
 		if (Input.IsActionPressed("rotate_left"))
 		{
 			ApplyTorque(new Vector3(0, 0, torque * (float)delta));
+			rightboosterParticles.Emitting = true;
 		}
+		else 
+		{ rightboosterParticles.Emitting = false;}
+
         if (Input.IsActionPressed("rotate_right"))
         {
             ApplyTorque(new Vector3(0, 0, -torque * (float)delta));
+			leftboosterParticles.Emitting = true;
         }
+		else
+		{  leftboosterParticles.Emitting = false;}
     }
 
     private Vector3 MultiplyByFloat(Vector3 left, float right)
@@ -74,6 +89,9 @@ public partial class Player : RigidBody3D
 	{
 		SetProcess(false);
 		explosionAudio.Play();
+		boosterParticles.Emitting = false;
+		rightboosterParticles.Emitting = false;
+		leftboosterParticles.Emitting = false;
 		Tween tween = CreateTween();
 		tween.TweenInterval(2.5f);
 		tween.TweenCallback(Callable.From(GetTree().ReloadCurrentScene));
